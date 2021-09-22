07212cef109c9669bd201ca5ba36c29936e89276
Richard McGirr <rmcgirr83@gmail.com>
2021-09-22 06:48:50 -0500

    Version 1.0.5

--- event/listener.php
+++ event/listener.php
@@ -13,6 +13,11 @@ namespace rmcgirr83\zodiacs\event;
 /**
 * @ignore
 */
+use phpbb\config\config;
+use phpbb\language\language;
+use phpbb\request\request;
+use phpbb\template\template;
+use phpbb\user;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -20,25 +25,30 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 */
 class listener implements EventSubscriberInterface
 {
-	/** @var config $config */
+	/** @var config */
 	protected $config;
 
-	/** @var request $request */
+	/** @var language */
+	protected $language;
+
+	/** @var request */
 	protected $request;
 
-	/** @var template $template */
+	/** @var template */
 	protected $template;
 
-	/** @var user $user */
+	/** @var user */
 	protected $user;
 
 	public function __construct(
-		\phpbb\config\config $config,
-		\phpbb\request\request $request,
-		\phpbb\template\template $template,
-		\phpbb\user $user)
+		config $config,
+		language $language,
+		request $request,
+		template $template,
+		user $user)
 	{
 		$this->config = $config;
+		$this->language = $language;
 		$this->request = $request;
 		$this->template = $template;
 		$this->user = $user;
@@ -53,7 +63,8 @@ class listener implements EventSubscriberInterface
 	*/
 	static public function getSubscribedEvents()
 	{
-		return array(
+		return [
+			'core.acp_extensions_run_action_after'		=> 'acp_extensions_run_action_after',
 			'core.viewtopic_cache_user_data'			=> 'viewtopic_cache_user_data',
 			'core.viewtopic_cache_guest_data'			=> 'viewtopic_cache_guest_data',
 			'core.viewtopic_modify_post_row'			=> 'viewtopic_modify_post_row',
@@ -61,7 +72,25 @@ class listener implements EventSubscriberInterface
 			'core.search_get_posts_data'				=> 'search_get_posts_data',
 			'core.search_modify_tpl_ary'				=> 'search_modify_tpl_ary',
 			'core.user_setup'							=> 'user_setup',
-		);
+		];
+	}
+
+	/* Display additional metdate in extension details
+	*
+	* @param $event			event object
+	* @param return null
+	* @access public
+	*/
+	public function acp_extensions_run_action_after($event)
+	{
+		if ($event['ext_name'] == 'rmcgirr83/zodiacs' && $event['action'] == 'details')
+		{
+			$this->language->add_lang('zodiacs', $event['ext_name']);
+			$this->template->assign_vars([
+				'L_BUY_ME_A_BEER_EXPLAIN'		=> $this->language->lang('BUY_ME_A_BEER_EXPLAIN', '<a href="' . $this->language->lang('BUY_ME_A_BEER_URL') . '" target="_blank" rel="noreferrer noopener">', '</a>'),
+				'S_BUY_ME_A_BEER_ZODIACS' => true,
+			]);
+		}
 	}
 
 	/**
@@ -82,16 +111,16 @@ class listener implements EventSubscriberInterface
 		$page_name = substr($this->user->page['page_name'], 0, strpos($this->user->page['page_name'], '.'));
 
 		// We only care about certain pages
-		if (in_array($page_name, array('viewtopic', 'memberlist', 'search')))
+		if (in_array($page_name, ['viewtopic', 'memberlist', 'search']))
 		{
 			$lang_set_ext = $event['lang_set_ext'];
-			$lang_set_ext[] = array(
+			$lang_set_ext[] = [
 				'ext_name' => 'rmcgirr83/zodiacs',
 				'lang_set' => 'zodiacs',
-			);
-			$this->template->assign_vars(array(
+			];
+			$this->template->assign_vars([
 				'S_ZODIACS' => true,
-			));
+			]);
 			$event['lang_set_ext'] = $lang_set_ext;
 		}
 	}
@@ -149,9 +178,9 @@ class listener implements EventSubscriberInterface
 
 		$zodiac = $this->get_user_zodiac($event['user_poster_data']['user_birthday']);
 
-		$event['post_row'] = array_merge($event['post_row'],array(
+		$event['post_row'] = array_merge($event['post_row'],[
 			'USER_ZODIAC' => $zodiac,
-		));
+		]);
 	}
 
 	/**
@@ -170,9 +199,9 @@ class listener implements EventSubscriberInterface
 
 		$zodiac = $this->get_user_zodiac($event['member']['user_birthday']);
 
-		$this->template->assign_vars(array(
+		$this->template->assign_vars([
 			'USER_ZODIAC'	=> $zodiac,
-		));
+		]);
 	}
 
 	/**
@@ -210,9 +239,9 @@ class listener implements EventSubscriberInterface
 
 		$array = $event['tpl_ary'];
 		$zodiac = $this->get_user_zodiac($event['row']['user_birthday']);
-		$array = array_merge($array, array(
+		$array = array_merge($array, [
 			'USER_ZODIAC'	=> $zodiac,
-		));
+		]);
 
 		$event['tpl_ary'] = $array;
 	}
@@ -230,20 +259,20 @@ class listener implements EventSubscriberInterface
 		{
 			list($bday, $bmonth) = array_map('intval', explode('-', $user_birthday));
 
-			$zodiac_array = array(
-				'aries'		=> array(3, 20, 4, 20),
-				'taurus'	=> array(4, 19, 5, 21),
-				'gemini'	=> array(5, 20, 6, 21),
-				'cancer'	=> array(6, 20, 7, 23),
-				'leo'		=> array(7, 22, 8, 23),
-				'virgo'		=> array(8, 22, 9, 23),
-				'libra'		=> array(9, 22, 10, 23),
-				'scorpio'	=> array(10, 22, 11, 22),
-				'sagittarius'	=> array(11, 21, 12, 22),
-				'capricorn'	=> array(12, 21, 1, 20),
-				'aquarius'	=> array(1, 19, 2, 19),
-				'pisces'	=> array(2, 18, 3, 21),
-			);
+			$zodiac_array = [
+				'aries'		=> [3, 20, 4, 20],
+				'taurus'	=> [4, 19, 5, 21],
+				'gemini'	=> [5, 20, 6, 21],
+				'cancer'	=> [6, 20, 7, 23],
+				'leo'		=> [7, 22, 8, 23],
+				'virgo'		=> [8, 22, 9, 23],
+				'libra'		=> [9, 22, 10, 23],
+				'scorpio'	=> [10, 22, 11, 22],
+				'sagittarius'	=> [11, 21, 12, 22],
+				'capricorn'	=> [12, 21, 1, 20],
+				'aquarius'	=> [1, 19, 2, 19],
+				'pisces'	=> [2, 18, 3, 21],
+			];
 
 			foreach ($zodiac_array as $sign => $date)
 			{
--- language/en/zodiacs.php
+++ language/en/zodiacs.php
@@ -14,7 +14,7 @@ if (!defined('IN_PHPBB'))
 
 if (empty($lang) || !is_array($lang))
 {
-	$lang = array();
+	$lang = [];
 }
 
 // DEVELOPERS PLEASE NOTE
@@ -29,7 +29,7 @@ if (empty($lang) || !is_array($lang))
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
-$lang = array_merge($lang, array(
+$lang = array_merge($lang, [
 	'ZODIAC'			=> 'Zodiac',
 	'AQUARIUS'			=> 'Aquarius',
 	'ARIES'				=> 'Aries',
@@ -43,4 +43,11 @@ $lang = array_merge($lang, array(
 	'SCORPIO'			=> 'Scorpio',
 	'TAURUS'			=> 'Taurus',
 	'VIRGO'				=> 'Virgo',
-));
+	//Donation
+	'PAYPAL_IMAGE_URL'          => 'https://www.paypalobjects.com/webstatic/en_US/i/btn/png/silver-pill-paypal-26px.png',
+	'PAYPAL_ALT'                => 'Donate using PayPal',
+	'BUY_ME_A_BEER_URL'         => 'https://paypal.me/RMcGirr83',
+	'BUY_ME_A_BEER'				=> 'Buy me a beer for creating this extension',
+	'BUY_ME_A_BEER_SHORT'		=> 'Make a donation for this extension',
+	'BUY_ME_A_BEER_EXPLAIN'		=> 'This extension is completely free. It is a project that I spend my time on for the enjoyment and use of the phpBB community. If you enjoy using this extension, or if it has benefited your forum, please consider <a href="https://paypal.me/RMcGirr83" target="_blank" rel="noreferrer noopener">buying me a beer</a>. It would be greatly appreciated. <i class="fa fa-smile-o" style="color:green;font-size:1.5em;" aria-hidden="true"></i>',	
+]);
